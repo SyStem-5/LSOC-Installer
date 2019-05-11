45401a5a8a893cc3deeb3b3afffc81ce7361c6aa
SyStem-5 <g1-assassin@hotmail.com>
2019-05-11 12:15:17 +0200

    Restructured build script Added comments Removed some commented-out code

--- build.sh
+++ build.sh
@@ -6,34 +6,35 @@ rm -rf $build_dir
 
 mkdir -p $build_dir
 
-rsync --info=progress2 ../LSOC-BlackBox/target/release/black_box $build_dir/blackbox/
-
-rsync --info=progress2 source/blackbox/blackbox.service   $build_dir/blackbox/
+# Base install/uninstall scripts
 rsync --info=progress2 source/install.sh        $build_dir
 rsync --info=progress2 source/uninstall.sh      $build_dir
+
+# BlackBox
+rsync --info=progress2 ../LSOC-BlackBox/target/release/black_box $build_dir/blackbox/
+rsync --info=progress2 source/blackbox/blackbox.service   $build_dir/blackbox/
+
+# Firewall
 rsync --info=progress2 source/ufw_setup/firewall_setup.sh $build_dir/ufw_setup/
 
-
+# Mosquitto
 rsync -a source/mosquitto                       $build_dir --exclude *.tar
-#git clone --recurse-submodules https://github.com/SyStem-5/Mosquitto-Auth-DockerImage.git $build_dir/mosquitto/mosquitto_docker
 rsync -a --info=progress2 ../Mosquitto-Auth-DockerImage/ $build_dir/mosquitto/mosquitto_docker \
     --exclude .vscode \
     --exclude .git \
     --exclude .gitignore \
     --exclude .gitmodules
-
 rsync -a --info=progress2 ../Mosquitto-Auth-Plugin/ $build_dir/mosquitto/mosquitto_docker/Mosquitto-Auth-Plugin \
     --exclude .vscode \
     --exclude .git \
     --exclude .gitignore \
     --exclude .gitmodules
 
-
+# Postgress
 rsync -a source/postgres                        $build_dir --exclude *.tar
 
-
+# Web Interface
 rsync -a --info=progress2 source/web_interface  $build_dir
-
 rsync -a --info=progress2 ../LSOC-WebInterface/ $build_dir/web_interface/webinterface_docker/django/app \
     --exclude .vscode \
     --exclude .git \
