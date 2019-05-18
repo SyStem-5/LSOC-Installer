9db80f41f241814042c70a0b985afe3e04165bd9
SyStem-5 <g1-assassin@hotmail.com>
2019-05-18 23:55:25 +0200

    Clean up the entry point script

--- source/web_interface/webinterface_docker/django/entrypoint.sh
+++ source/web_interface/webinterface_docker/django/entrypoint.sh
@@ -4,10 +4,7 @@ echo "Waiting for postgres..."
 while ! nc -z $SQL_HOST $SQL_PORT; do sleep 1; done
 echo "PostgreSQL is up - continuing execution."
 
-#tree -a /usr/src/djangoapp/
-
 cd /usr/src/djangoapp
-#python manage.py flush --no-input
 
 python manage.py makemigrations
 python manage.py migrate
