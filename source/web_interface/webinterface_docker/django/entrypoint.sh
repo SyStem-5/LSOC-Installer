75dac176810357898eaa11297a209c07478211b2
SyStem-5 <g1-assassin@hotmail.com>
2019-05-18 23:54:46 +0200

    Cleaner way of waiting for postgres to go online.

--- source/web_interface/webinterface_docker/django/entrypoint.sh
+++ source/web_interface/webinterface_docker/django/entrypoint.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
+echo "Waiting for postgres..."
+while ! nc -z $SQL_HOST $SQL_PORT; do sleep 1; done
+echo "PostgreSQL is up - continuing execution."
 
 #tree -a /usr/src/djangoapp/
 
