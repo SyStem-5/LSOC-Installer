36d509886f7743452f3bb4fbf3a1282d67870774
SyStem-5 <g1-assassin@hotmail.com>
2019-06-28 18:11:32 +0200

    Improved the neco install script

--- source/neutron_communicator/install.sh
+++ source/neutron_communicator/install.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
-echo -e "\e[1m\e[44mLSOC Installer\e[0m: Installing Neutron Communicator"
+# Set the workdir to the directory the script is in
+cd "$(dirname "$0")"
 
-bin_name=neutron_communicator
-bin_loc_source=neutron_communicator/$bin_name
-bin_loc_dest=/bin/$bin_name
-config_loc=/etc/NeutronCommunicator
+echo -e "\e[1m\e[45mNeutron Communicator Installer\e[0m: Starting Neutron Communicator Installation"
 
-# Binary copying and permissions
-cp $bin_loc_source /bin/
-chown root:root $bin_loc_dest
-chmod 700 $bin_loc_dest
+neco_gen_settings_cmd=gen_settings
+binary_name=neutron_communicator
+binary_destination=/bin/$binary_name
+service_file_destination=/etc/systemd/system
+service_file_name=neutroncommunicator.service
+config_dir=/etc/NeutronCommunicator
 
-# Configuration dir permissions
-mkdir $config_loc
-chown root:root $config_loc
-chmod 600 $config_loc
+echo -e "\e[1m\e[45mNeutron Communicator Installer\e[0m: Copying the binary..."
+cp $binary_name /bin/
 
-read -p $'\e[1m\e[44mLSOC Installer\e[0m: Default NECO settings file is going to be generated, please edit the file responsibly. To continue press [ENTER] ' -r
-$bin_name gen_settings
-nano $config_loc/settings.json
+echo -e "\e[1m\e[45mNeutron Communicator Installer\e[0m: Setting permissions..."
+chown root:root $binary_destination
+chmod 700 $binary_destination
 
-# Service setup
-cp neutron_communicator/neutroncommunicator.service /etc/systemd/system/
-chmod 750 /etc/systemd/system/neutroncommunicator.service
-systemctl enable neutroncommunicator.service
+echo -e "\e[1m\e[45mNeutron Communicator Installer\e[0m: Creating the configuration directory..."
+mkdir $config_dir
 
-echo -e "\e[1m\e[44mLSOC Installer\e[0m: NECO installation complete."
+echo -e "\e[1m\e[45mNeutron Communicator Installer\e[0m: Setting permissions..."
+chown root:root $config_dir
+chmod 600 $config_dir
+
+read -p $'\e[1m\e[45mNeutron Communicator Installer\e[0m: Default NECO settings file is going to be generated, please edit the file responsibly. To continue press [ENTER] ' -r
+$binary_name $neco_gen_settings_cmd
+nano $config_dir/settings.json
+
+echo -e "\e[1m\e[45mNeutron Communicator Installer\e[0m: Copying the service file..."
+cp $service_file_name $service_file_destination
+
+echo -e "\e[1m\e[45mNeutron Communicator Installer\e[0m: Setting permissions..."
+chmod 750 $service_file_destination/$service_file_name
+
+echo -e "\e[1m\e[45mNeutron Communicator Installer\e[0m: Enabling service..."
+systemctl enable $service_file_name
+
+echo -e "\e[1m\e[45mNeutron Communicator Installer\e[0m: NECO installation complete."
