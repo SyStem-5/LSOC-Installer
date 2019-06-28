a6d80bb7914235fb10951863a02bbc513193205b
SyStem-5 <g1-assassin@hotmail.com>
2019-06-28 19:46:47 +0200

    Improved the postgresql install script

--- source/postgres/docker_run_postgresql.sh
+++ source/postgres/docker_run_postgresql.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-version_file=/etc/BlackBox/postgresql.version
-
-echo '0.1.0' > $version_file
-
-# Remove \n from the end
-truncate -s -1 $version_file
-
 sudo docker container start database_postgres
