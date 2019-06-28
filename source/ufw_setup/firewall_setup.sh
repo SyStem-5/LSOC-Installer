5e2fed98e9775b53872a961df09cc316ec74a8b2
SyStem-5 <g1-assassin@hotmail.com>
2019-06-28 18:23:21 +0200

    Improved the ufw script

--- source/ufw_setup/firewall_setup.sh
+++ source/ufw_setup/firewall_setup.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-echo -e "\e[1m\e[44mLSOC Installer\e[0m: Setting up firewall..."
+read -p $'\e[1m\e[45mUFW Setup\e[0m: Set up the Uncomplicated Firewall? [Y/n] ' -r REPLY
+REPLY=${REPLY:-y}
+echo    #Move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    # handle exits from shell or function but don't exit interactive shell
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
 
-echo -e "\e[1m\e[44mLSOC Installer\e[0m: Denying all incoming network traffic by default"
+echo -e "\e[1m\e[45mUFW Setup\e[0m: Setting up firewall..."
 
+echo -e "\e[1m\e[45mUFW Setup\e[0m: Denying all incoming network traffic."
 ufw default deny incoming
 
-echo -e "\e[1m\e[44mLSOC Installer\e[0m: Setting firewall rules..."
+echo -e "\e[1m\e[45mUFW Setup\e[0m: Allowing all outgoing network traffic."
+ufw default allow outgoing
 
+echo -e "\e[1m\e[45mUFW Setup\e[0m: Setting firewall rules..."
 #Nginx
 ufw allow https
 ufw allow http
-
 #MQTT Secure
 ufw allow 8883/tcp
+#SSH
+ufw allow ssh
 
-echo -e "\e[1m\e[44mLSOC Installer\e[0m: Enabling firewall..."
-
+echo -e "\e[1m\e[45mUFW Setup\e[0m: Enabling firewall..."
 ufw enable
