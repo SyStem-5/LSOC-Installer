8151c5c696115186e72e70c943bae3e969b19edd
SyStem-5 <g1-assassin@hotmail.com>
2019-06-28 19:47:29 +0200

    Improved the postgresql install script

--- source/postgres/install.sh
+++ source/postgres/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-read -p $'\e[1m\e[44mLSOC Installer\e[0m: Install PostgreSQL? [Y/n] ' -r REPLY
+# Set the workdir to the directory the script is in
+cd "$(dirname "$0")"
+
+read -p $'\e[1m\e[45mPostgreSQL Installer\e[0m: Install PostgreSQL? [Y/n] ' -r REPLY
 REPLY=${REPLY:-y}
 echo    #Move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
@@ -8,40 +11,39 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
 fi
 
-# We can create it here, it doesn't matter if it fails or doesn't, permissions will be set anyway
-bb_config_base_loc=/etc/BlackBox/
-mkdir $bb_config_base_loc
-
-postgres_package=postgres:11-alpine
-postgres_container_local=postgres/postgres.tar
-#Generate the password for postgresql(docker image) and save it to /etc/BlackBox name: postgresql_bb.creds
-creds=$(openssl rand -base64 32)
-echo $creds >> $bb_config_base_loc/postgresql_bb.creds
-
-#Copy the script for running postgresql docker container
-cp postgres/docker_run_postgresql.sh $bb_config_base_loc
-
-#If the local container exists, we ask to install local or download the container from the internet
-if [ -f $postgres_container_local ]; then
-    read -p $'\e[1m\e[45mPostgreSQL Installer\e[0m: Download the PostgreSQL docker container? [Y/n] ' -r
-    REPLY=${REPLY:-y}
-    echo    #Move to a new line
-    if [[ $REPLY =~ ^[Yy]$ ]] ; then
-        docker pull $postgres_package
-    else
-        #Load the docker container
-        docker load < $postgres_container_local
-    fi
-else
-    echo -e "\e[1m\e[45mPostgreSQL Installer\e[0m: Downloading PostgreSQL container."
-    docker pull $postgres_package
+# If no arguments are specified, exit
+if [ -z "$1" ]; then
+    echo "No configuration base directory specified. Exiting..."
+    exit 1
 fi
 
-#Create the network so other containers can connect
+config_base_location=$1
+creds_file_loc=$config_base_location/postgresql_bb.creds
+pg_major_version=11
+postgres_package=postgres:$pg_major_version-alpine
+
+if [ ! -d "$config_base_location" ]; then
+    echo -e "\e[1m\e[45mPostgreSQL Installer\e[0m: Creating base configuration directory. Path: $config_base_location."
+    mkdir $config_base_location
+fi
+
+echo -e "\e[1m\e[45mPostgreSQL Installer\e[0m: Generating and saving a version file. v$pg_major_version.0.0"
+echo $pg_major_version.0.0 > $config_base_location/postgresql.version
+
+echo -e "\e[1m\e[45mPostgreSQL Installer\e[0m: Generating and saving postgres credentials..."
+creds=$(openssl rand -base64 32)
+echo $creds > $creds_file_loc
+
+echo -e "\e[1m\e[45mPostgreSQL Installer\e[0m: Copying docker run script to the base configuration directory..."
+echo \
+'#!/bin/bash
+sudo docker container start database_postgres' \
+> $config_base_location/docker_run_postgresql.sh
+
+echo -e "\e[1m\e[45mPostgreSQL Installer\e[0m: Creating the docker database network..."
 docker network create database
 
 #On first run; Run the PostgreSQL docker image with the password from file in /etc/BlackBox and as "postgresql" user pointing to the database file in /etc/postgres
-creds_file_loc=/etc/BlackBox/postgresql_bb.creds
 password=$(<$creds_file_loc);
 docker run --restart on-failure -d \
     -p 127.0.0.1:5432:5432 \
@@ -51,6 +53,6 @@ docker run --restart on-failure -d \
     -e POSTGRES_PASSWORD=$password $postgres_package
 
 #Make crontab start the script(as root) on reboot so it starts even when no one is logged in
-(crontab -l 2>/dev/null; echo "@reboot /bin/sh /etc/BlackBox/docker_run_postgresql.sh") | crontab -
+(crontab -l 2>/dev/null; echo "@reboot /bin/sh $config_base_location/docker_run_postgresql.sh") | crontab -
 
 echo -e "\e[1m\e[45mPostgreSQL Installer\e[0m: Installation Complete."
