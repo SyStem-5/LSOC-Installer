65970579b195d5c61b2c6fa34b4cab1b58d6c53f
SyStem-5 <g1-assassin@hotmail.com>
2019-06-29 22:12:29 +0200

    Improved WebInterface install script

--- source/web_interface/install.sh
+++ source/web_interface/install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-read -p $'\e[1m\e[44mLSOC Installer\e[0m: Install BlackBox Web Interface? [Y/n] ' -r REPLY
+# Set the workdir to the directory the script is in
+cd "$(dirname "$0")"
+
+# If no arguments are specified, exit
+if [ -z "$1" ]; then
+    echo "No configuration base directory specified. Exiting..."
+    exit 1
+fi
+
+read -p $'\e[1m\e[45mWeb Interface Installer\e[0m: Install the Web Interface? [Y/n] ' -r REPLY
 REPLY=${REPLY:-y}
 echo    #Move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
@@ -8,51 +17,51 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
 fi
 
-bb_config_base_loc=/etc/BlackBox/
+config_base_loc=$1
+deployed_dir_name=webinterface_docker
 
-echo -e "\e[1m\e[45mWeb Interface Installer\e[0m: Installing docker-compose."
-
+echo -e "\e[1m\e[45mWeb Interface Installer\e[0m: Installing docker-compose..."
 apt install -y docker-compose
 
-#Copy the script for running WI docker container
-cp web_interface/docker_run_webinterface.sh $bb_config_base_loc
+echo -e "\e[1m\e[45mWeb Interface Installer\e[0m: Copying docker run command file..."
+cp docker_run_webinterface.sh $config_base_loc
 
-#Copy the docker app to $bb_config_base_loc
-cp -r web_interface/webinterface_docker $bb_config_base_loc
+echo -e "\e[1m\e[45mWeb Interface Installer\e[0m: Deploying the webapp to the base directory..."
+cp -r $deployed_dir_name $config_base_loc
 
-cd $bb_config_base_loc
+echo -e "\e[1m\e[45mWeb Interface Installer\e[0m: Setting permissions..."
+chown -R root:root $config_base_loc/$deployed_dir_name
+chmod -R 740 $config_base_loc/$deployed_dir_name
 
-chown root:root webinterface_docker
-chmod 740 webinterface_docker
+echo -e "\e[1m\e[45mWeb Interface Installer\e[0m: Generating the secret key for the web-app..."
+#secret_key=$(openssl rand -base64 55)
+echo $(openssl rand -base64 55) > $config_base_loc/$deployed_dir_name/secret_key.txt
 
-cd webinterface_docker/
+echo -e "\e[1m\e[45mWeb Interface Installer\e[0m: Generating database credentials..."
+echo "postgres" > $config_base_loc/$deployed_dir_name/sql_user.txt
+#sql_pass=$(openssl rand -base64 32)
+echo $(openssl rand -base64 32) > $config_base_loc/$deployed_dir_name/sql_pass.txt
 
-secret_key=$(openssl rand -base64 55)
-echo $secret_key >> secret_key.txt
-
-echo "postgres" >> sql_user.txt
-sql_pass=$(openssl rand -base64 32)
-echo $sql_pass >> sql_pass.txt
-
+echo -e "\e[1m\e[45mWeb Interface Installer\e[0m: Generating self-signed SSL certificate..."
 openssl req \
-       -newkey rsa:2048 -nodes -keyout site.key \
-       -x509 -days 750 -out site.crt \
+       -newkey rsa:2048 -nodes -keyout $config_base_loc/$deployed_dir_name/site.key \
+       -x509 -days 750 -out $config_base_loc/$deployed_dir_name/site.crt \
        -subj "/C=HR/ST=Croatia"
 
-echo -e "\e[1m\e[45mWeb Interface Installer\e[0m: Loading Docker container."
-
-docker-compose up -d --build
+echo -e "\e[1m\e[45mWeb Interface Installer\e[0m: Building and running Docker image."
+docker-compose -f $config_base_loc/$deployed_dir_name/docker-compose.yml up -d --build
 
 read -p $'\e[1m\e[45mWeb Interface Installer\e[0m: Set Web Interface Username. Press [ENTER] for [admin] ' -r username
 username=${username:-admin}
 
 read -p $'\e[1m\e[45mWeb Interface Installer\e[0m: Set Web Interface Email. Press [ENTER] to skip. ' -r email
 
-pass=$(openssl rand -base64 32)
 
-echo "Waiting for WebInterface to go online..."
-
+echo -e "\e[1m\e[45mWeb Interface Installer\e[0m: Waiting 60sec for WebInterface to go online..."
 sleep 60
+
+# Generate the password for the superuser web interface account
+pass=$(openssl rand -base64 32)
 
 docker exec -i -t $(sudo docker ps -aqf "name=lsoc_webinterface_django") /bin/ash -c \
 "echo \"from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$username', '$email', '$pass')\" | python manage.py shell"
@@ -60,6 +69,6 @@ docker exec -i -t $(sudo docker ps -aqf "name=lsoc_webinterface_django") /bin/as
 read -p $'\e[1m\e[45mWeb Interface Installer\e[0m: \e[4mIT IS NOT RECOMMENDED TO KEEP A DIGITAL COPY OF THIS PASSWORD!\e[0m \n Web Interface superuser password: '"[$pass]."$'\nPress [ENTER] to continue. ' -r
 
 #Make crontab start the script(as root) on reboot so it starts even when no one is logged in
-(crontab -l 2>/dev/null; echo "@reboot /bin/sh /etc/BlackBox/docker_run_webinterface.sh") | crontab -
+(crontab -l 2>/dev/null; echo "@reboot /bin/sh $config_base_loc/docker_run_webinterface.sh") | crontab -
 
 echo -e "\e[1m\e[45mWeb Interface Installer\e[0m: Installation Complete."
